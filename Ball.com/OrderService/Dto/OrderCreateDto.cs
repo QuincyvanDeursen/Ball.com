ee9b028a5ca4d3d4e6ada1cb57311e64aaab5a83
Lars <ljaw.jongenelen@student.avans.nl>
2025-06-25 17:35:34 +0200

    Added Dtos

---
+++ Ball.com/OrderService/Dto/OrderCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using OrderService.Domain;
+
+namespace OrderService.Dto
+{
+	public class OrderCreateDto
+	{
+		public Guid CustomerId { get; set; }
+		public List<OrderItemCreateDto> OrderItems { get; set; } = new();
+
+	}
+
+	public class OrderItemCreateDto
+	{
+		public Guid ItemId { get; set; }
+		public int Quantity { get; set; }
+	}
+}
---
+++ Ball.com/OrderService/Dto/OrderUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using OrderService.Domain;
+
+namespace OrderService.Dto
+{
+	public class OrderUpdateDto
+	{
+		public Guid OrderId { get; set; }
+		public PaymentStatus? PaymentStatus { get; set; }
+		public OrderStatus? OrderStatus { get; set; }
+	}
+}
